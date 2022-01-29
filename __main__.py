fb90fc93593ac302a96e17278dbe335bd05ea408
ambuj <pawar.ambuj@gmail.com>
2022-01-29 14:54:37 +0100

    Renove unnecessary imports

--- __main__.py
+++ __main__.py
@@ -1,6 +1,3 @@
-from tkinter import mainloop
-from unittest.main import MAIN_EXAMPLES
-from pip import main
 from utils.read_input import ReadInput
 
 if __name__ == '__main__':
@@ -8,3 +5,4 @@ if __name__ == '__main__':
     filepath = "/Users/ambuj/Desktop/PycharmProject1/data/WhatsAppChat-Asha_part1.txt"
     conversations = ReadInput(filepath)
     print("DONE")
+
