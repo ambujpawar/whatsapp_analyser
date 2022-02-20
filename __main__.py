8166c336f5bc50d4c99f3268b2752bdd59aeaa17
ambuj <pawar.ambuj@gmail.com>
2022-02-20 22:55:27 +0100

    Remove unnecesaary info from main.py

--- __main__.py
+++ __main__.py
@@ -5,27 +5,4 @@ from utils.wordcloud_utils import PrepareDataForWordcloud, AddCustomStopwords, D
 
 
 if __name__ == '__main__':
-    # Read the input file
-    filepath = "/Users/ambuj/Desktop/PycharmProject1/data/WhatsAppChat-Asha_part2.txt"
-    stopwords_filepath = "/Users/ambuj/Desktop/GithubProjects/whatsapp_analyser/data/stopwords.txt"
-    n = 5
-
-    conversations = ReadInput(filepath)
-
-    #statistics = GetMessageStatistics(conversations)
-    #time_statistics = GetMessagesByDay(conversations)
-    
-    custom_stopwords = AddCustomStopwords(stopwords_filepath)
-    data = PrepareDataForWordcloud(conversations, for_every_user=True)
-    DrawWordCloud(data['asha nit'] + data['ambuj'], stopwords=custom_stopwords)
-    
-    """
-    emoji_count = EmojiCount(conversations, n=5)
-    for user, emojis in emoji_count.items():
-        print('The {} most common emoji used by {} are: '.format(n, user))
-        most_common_emojis = emojis.most_common(n)
-        for emoji in most_common_emojis:
-            print(emoji)
-    """
-
     print("DONE")
